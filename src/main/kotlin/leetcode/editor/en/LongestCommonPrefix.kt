03703708e0f32577e09f07d7822fb85145a70223
anderson-spider <andermalk16@gmail.com>
2025-07-17 14:39:56 -0300

    feat: initialize project with Gradle Kotlin DSL and Leetcode solutions configuration

---
+++ src/main/kotlin/leetcode/editor/en/LongestCommonPrefix.kt
@@ -0,0 +1,68 @@
+package leetcode.editor.en
+
+import kotlin.test.assertEquals
+
+//Write a function to find the longest common prefix string amongst an array of
+//strings. 
+//
+// If there is no common prefix, return an empty string "". 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: strs = ["flower","flow","flight"]
+//Output: "fl"
+// 
+//
+// Example 2: 
+//
+// 
+//Input: strs = ["dog","racecar","car"]
+//Output: ""
+//Explanation: There is no common prefix among the input strings.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= strs.length <= 200 
+// 0 <= strs[i].length <= 200 
+// strs[i] consists of only lowercase English letters if it is non-empty. 
+// 
+//
+// Related Topics Array String Trie 👍 19497 👎 4764
+
+object LongestCommonPrefix {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val solution = Solution()
+        assertEquals("fl", solution.longestCommonPrefix(arrayOf("flower", "flow", "flight")))
+        assertEquals("", solution.longestCommonPrefix(arrayOf("dog", "racecar", "car")))
+        assertEquals("", solution.longestCommonPrefix(arrayOf()))
+        assertEquals("a", solution.longestCommonPrefix(arrayOf("a")))
+        assertEquals("c", solution.longestCommonPrefix(arrayOf("c", "c")))
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        fun longestCommonPrefix(strs: Array<String>): String {
+            if (strs.isEmpty()) return ""
+            if (strs.size == 1) return strs[0]
+
+            var prefix = strs[0]
+            for (i in 1 until strs.size) {
+                while (strs[i].indexOf(prefix) != 0) {
+                    prefix = prefix.substring(0, prefix.length - 1)
+                    if (prefix.isEmpty()) return ""
+                }
+            }
+            return prefix
+        }
+    }
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
