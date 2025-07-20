7fac9f63afcb2d1dc371ae62a3f643d081a85bb2
Anderson Silva <anderson.silva@luizalabs.com>
2025-07-20 07:38:16 -0300

    feat: add solution for Reverse Integer problem

---
+++ src/main/kotlin/leetcode/editor/en/ReverseInteger.kt
@@ -0,0 +1,87 @@
+package leetcode.editor.en
+
+import kotlin.math.abs
+import kotlin.test.assertEquals
+
+//Given a signed 32-bit integer x, return x with its digits reversed. If 
+//reversing x causes the value to go outside the signed 32-bit integer range [-2³¹, 2³¹ -
+// 1], then return 0. 
+//
+// Assume the environment does not allow you to store 64-bit integers (signed 
+//or unsigned). 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: x = 123
+//Output: 321
+// 
+//
+// Example 2: 
+//
+// 
+//Input: x = -123
+//Output: -321
+// 
+//
+// Example 3: 
+//
+// 
+//Input: x = 120
+//Output: 21
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// -2³¹ <= x <= 2³¹ - 1 
+// 
+//
+// Related Topics Math 👍 14345 👎 13806
+
+
+object ReverseInteger {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val solution = Solution()
+        val case1 = solution.reverse(x = 123)
+        assertEquals(321, case1)
+
+        val case2 = solution.reverse(x = -123)
+        assertEquals(-321, case2)
+
+        val case3 = solution.reverse(x = 120)
+        assertEquals(21, case3)
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        fun reverse(x: Int): Int {
+            val negative = x < 0
+            var num = abs(x.toLong())
+            var revNum = 0L
+
+            while (num != 0L) {
+                val factor = 10L
+                val mod = num % factor
+                num = num / factor
+                revNum = revNum * factor + mod
+            }
+
+            return if (revNum > Int.MAX_VALUE) {
+                0
+            } else if (negative) {
+                0 - revNum.toInt()
+            } else {
+                revNum.toInt()
+            }
+
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+
+}
