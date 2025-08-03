549529635a667f927169cd02ad41df6c9b8987e6
anderson-spider <andermalk16@gmail.com>
2025-08-03 11:46:47 -0300

    feat: add NumberOf1Bits problem solution with examples and documentation
    - Implemented the `hammingWeight` method to count the number of set bits in a binary number using bit manipulation.
    - Included problem description, examples, constraints, and related topics in the documentation (`NumberOf1Bits.md`).
    - Added test cases to validate the solution under various scenarios.
    - Updated project configuration (`editor.xml`) to include the new problem.

---
+++ src/main/kotlin/leetcode/editor/en/NumberOf1Bits.kt
@@ -0,0 +1,81 @@
+package leetcode.editor.en
+// Given a positive integer n, write a function that returns the number of set
+// bits in its binary representation (also known as the Hamming weight).
+//
+//
+// Example 1:
+//
+//
+// Input: n = 11
+//
+//
+// Output: 3
+//
+// Explanation:
+//
+// The input binary string 1011 has a total of three set bits.
+//
+// Example 2:
+//
+//
+// Input: n = 128
+//
+//
+// Output: 1
+//
+// Explanation:
+//
+// The input binary string 10000000 has a total of one set bit.
+//
+// Example 3:
+//
+//
+// Input: n = 2147483645
+//
+//
+// Output: 30
+//
+// Explanation:
+//
+// The input binary string 1111111111111111111111111111101 has a total of
+// thirty set bits.
+//
+//
+// Constraints:
+//
+//
+// 1 <= n <= 2³¹ - 1
+//
+//
+//
+// Follow up: If this function is called many times, how would you optimize it?
+//
+// Related Topics Divide and Conquer Bit Manipulation 👍 6898 👎 1354
+
+object NumberOf1Bits {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val solution = Solution()
+
+        check(solution.hammingWeight(11) == 3) { "Failed test case 1: Input 11 should have 3 set bits" }
+        check(solution.hammingWeight(128) == 1) { "Failed test case 2: Input 128 should have 1 set bit" }
+        check(solution.hammingWeight(2147483645) == 30) { "Failed test case 3: Input 2147483645 should have 30 set bits" }
+
+        println("All test cases passed!")
+    }
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        fun hammingWeight(n: Int): Int {
+            var n = n
+            var res = 0
+            while (n > 0) {
+                res += n and 1
+                n = n shr 1
+            }
+
+            return res
+        }
+    }
+    // leetcode submit region end(Prohibit modification and deletion)
+}
